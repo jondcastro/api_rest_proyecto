836be539139af0da48d3bc2e98fd96b47ce7f65b
jonathan <jonathancastro2018@yahoo.com>
2019-11-17 20:03:29 +0100

    creando estrcutura del proyecto

---
+++ server.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('blogis', 'root', '', {
+  host: 'localhost',
+  dialect:'mysql'
+});
+
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
