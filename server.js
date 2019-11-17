9139b128db90f1d7199cabea82b90ed8ca943630
jonathan <jonathancastro2018@yahoo.com>
2019-11-17 20:25:40 +0100

    separando servidor y conexion

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const Sequelize = require('sequelize');
+var express = require('express');
+var app = express();
 
-// Option 1: Passing parameters separately
-const sequelize = new Sequelize('blogis', 'root', '', {
-  host: 'localhost',
-  dialect:'mysql'
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+module.exports = app;
